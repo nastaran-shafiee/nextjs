5e43189544f812c81c054b3b70fd1388e090a0ff
n.shafie <n.shafie@gmail.com>
2024-07-21 11:21:05 +0330

    make not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+export default function NotFound(){
+    return <div className="flex justify-center items-center w-full min-h-screen">
+        <h1 className="text-red-300">page not found</h1>
+    </div>
+}
